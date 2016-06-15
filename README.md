bb6f852088f3533f81226f8fe064889dcaa32926
olli vainola <olli.vainola@student.oulu.fi>
2016-06-15 20:29:45 +0300

    Added dummy tests

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 
 ## WebPoller
+
+A small implementation of a program that monitors web sites and reports their
+availability. This tool is intended as a monitoring tool for web site
+administrators for detecting problems on their sites.
+
+The program has a browser interface, which is created via [pyramid](http://www.pylonsproject.org/). The python class, which polls
+for sites and their availability is created using requests & threading.
--- tests/test_poller.py
+++ tests/test_poller.py
@@ -1,4 +1,5 @@
 from nose.tools import *
+import unittest
 import os
 import sys
 
@@ -8,3 +9,41 @@ previous_folder = "/".join(curr_path.split('/')[:-2])
 sys.path.append(previous_folder)
 print(previous_folder)
 from poller import *
+
+class WebPollerTester(unittest.TestCase):
+
+    def test_logger_webpoller_factory(self):
+        """ TODO """
+        pass
+
+    def test_check_settings_sections(self):
+        """ TODO """
+        pass
+
+    def test_time_decorator(self):
+        """ TODO """
+        pass
+
+    def test_get_html(self):
+        """ TODO """
+        pass
+
+    def test_check_rule_output(self):
+        """ TODO """
+        pass
+
+    def test_WebPoller_create(self):
+        """ TODO """
+        pass
+
+    def test_WebPoller_run(self):
+        """ TODO """
+        pass
+
+    def test_WebPoller_logger(self):
+        """ TODO """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
